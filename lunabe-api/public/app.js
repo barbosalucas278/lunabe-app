3b9c121bf6ca3cf064d90c9b88244d13a23b2bcb
barbosalucas278 <barbosalucasroberto@gmail.com>
2020-12-27 19:29:20 -0300

    prueba js

--- lunabe-api/public/app.js
+++ lunabe-api/public/app.js
@@ -1,1 +1,6 @@
-alert("Hola mundo");
+const btnSaludo = document
+  .querySelector("#saludo")
+  .addEventListener("click", function () {
+    console.log("hola");
+    alert("Hola Martinaaaa!");
+  });
