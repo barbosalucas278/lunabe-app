4b5e97f81a39c14bc0ead9b43eb850a49df0a232
barbosalucas278 <barbosalucasroberto@gmail.com>
2021-02-23 15:05:22 -0300

    probando 2

---
+++ js/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $(".container").on("click", function () {
+    alert("Hola Mundo");
+  });
+});
--- lunabe-api/public/app.js
+++
@@ -1,6 +0,0 @@
-const btnSaludo = document
-  .querySelector("#saludo")
-  .addEventListener("click", function () {
-    console.log("hola");
-    alert("Hola Martinaaaa!");
-  });
